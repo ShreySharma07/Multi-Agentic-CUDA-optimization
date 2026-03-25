a1971e520c1d1e00229f38cacfcba28cde6d5725
Shrey Sharma <129565452+ShreySharma07@users.noreply.github.com>
2026-03-25 14:01:34 +0530

    updated the compiler.py to handle GPU architecture on the fly and included -O3 (maximum compiler optimization), -arch in the command line list

--- pipeline/compiler.py
+++ pipeline/compiler.py
@@ -1,18 +1,24 @@
 import subprocess
 from pathlib import Path
+import torch
+
+def get_gpu_info():
+    major, minor = torch.cuda.get_device_capability()
+    return f"sm_{major}{minor}"
 
 def compile_cuda(file_path):
 
     executable = file_path.replace(".cu", "")
 
-    cmd = ['nvcc', str(file_path), '-o', executable]
+    arch_type = get_gpu_info()
+
+    cmd = ['nvcc', str(file_path), '-o', executable, '-O3', f'-arch={arch_type}', '-lineinfo']
 
     process = subprocess.run(cmd, capture_output=True, text=True)
 
     if process.returncode == 0:
         print("Compilation successful")
-        return executable
+        return (True, file_path)
     else:
         print("Compilation Failed!")
-        print(process.stderr)
-        return None
+        return (False, process.stderr)
